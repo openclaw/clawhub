b49665ef6123e2850c32c7a2fcf6162812183ba4
Val Alexander <bunsthedev@gmail.com>
2026-04-20 20:53:11 -0500

    fix: tighten og image layout

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,7 +10,7 @@ import Header from "../components/Header";
 import { getSiteDescription, getSiteMode, getSiteName, getSiteUrlForMode } from "../lib/site";
 import appCss from "../styles.css?url";
 
-const OG_IMAGE_VERSION = "20260420-7";
+const OG_IMAGE_VERSION = "20260420-8";
 
 export const Route = createRootRoute({
   head: () => {
