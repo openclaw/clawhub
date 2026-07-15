770c2aea2562f937790d5881b3e0510b52200fd9
Gio Della-Libera <giodl@microsoft.com>
2026-07-15 07:40:07 -0700

    feat(feeds): sign stored catalog publications

--- convex/http.ts
+++ convex/http.ts
@@ -51,7 +51,6 @@ import {
   listPromotionsV1Http,
   promotionsGetRouterV1Http,
   promotionsPostRouterV1Http,
-  catalogFeedV1Http,
   catalogSkillsFeedV1Http,
   promotionsFeedV1Http,
   usersGetRouterV1Http,
@@ -63,6 +62,7 @@ import {
   skillsShCatalogTestV1Http,
   skillsShCatalogPublicV1Http,
 } from "./httpApiV1";
+import { signedCatalogFeedV1Http } from "./httpApiV1/catalogFeedSigning";
 import { preflightHandler } from "./httpPreflight";
 import { installRateLimitedRoutes } from "./lib/httpRouteRateLimit";
 import {
@@ -175,7 +175,7 @@ http.route({
 http.route({
   path: ApiRoutes.catalogFeed,
   method: "GET",
-  handler: catalogFeedV1Http,
+  handler: signedCatalogFeedV1Http,
 });
 
 http.route({
